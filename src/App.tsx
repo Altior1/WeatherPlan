bbd879b747cd742015ad0fc50989e7125cf9b72e
Altior1 <thomas.rud@outlook.fr>
2025-11-28 09:58:08 +0100

    Project-injection du contexte de filtre

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
 import './App.css'
+import { useReducer } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import FetchCountries from './components/FetchCountries';
 import Header from './components/Header';
+import { FilterCountryContext } from './contexts/FilterCountryContext';
 
+const initialFilterState: string | null = null;
+
+const filterReducer = (state: string | null, action: { type: string; payload?: string | null }) => {
+    switch (action.type) {
+      case 'SET_FILTER':
+        return action.payload || null;
+      case 'CLEAR_FILTER':
+        return null;
+      default:
+        return state;
+    }
+  };
 
 function App() {
+  const [filterCountry, dispatch] = useReducer(filterReducer, initialFilterState);
+
+
   return (
     <>
       <Header />
+      <FilterCountryContext.Provider value={filterCountry}>
       <Routes>
         <Route path="/all_countries" element={<FetchCountries />} />
-      </Routes>
+        </Routes>
+        </FilterCountryContext.Provider>
     </>
   )
 }
